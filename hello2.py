21612729bc05626de4adab3ac4eacdb016bd405e
Asad Moin <amoin@magento.com>
2018-07-28 18:01:16 -0500

    Fixed counter

--- hello2.py
+++ hello2.py
@@ -3,9 +3,11 @@ import time
 counter = 0
 
 while True:
-    print("Execution time in minutes: " + counter)
+    print("Execution time in minutes: " + str(counter))
     time.sleep(60) # Delay for 1 minute (60 seconds).
     counter = counter + 1
     
     if counter == 55:
         break
+
+print('Out of loop')
